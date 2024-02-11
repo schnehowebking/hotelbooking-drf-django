d0c8cca6b3fe2ddd2718fa01cde1d1d28c6945eb
schnehowebking <schnehowebking@gmail.com>
2024-02-12 01:57:49 +0600

    updated projects

--- isthotelbooking/settings.py
+++ isthotelbooking/settings.py
@@ -57,6 +57,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
